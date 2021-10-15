b0e8b4a05a9ea3a6278fc9b2cc5762cb9466dd69
Spike Liao <lpylpyleo@gmail.com>
2021-10-15 14:03:04 +0800

    wip

--- library/email/email.go
+++ library/email/email.go
@@ -13,6 +13,9 @@ const (
 func SendMail(to, subject, msg string) error {
 	body := fmt.Sprintf("To: %s\r\n"+
 		"Subject: %s\r\n"+
+		"MIME-version: 1.0;\r\n"+
+		"Content-Type: text/html;"+
+		"charset=\"UTF-8\";\r\n"+
 		"\r\n"+
 		"%s\r\n", to, subject, msg)
 	return smtp.SendMail(smtpServer, nil, mailNoReply, []string{to}, []byte(body))
